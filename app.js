bc52ee1e914b1da5ab1e35e50393f96ce087716c
FoxWithMatches <freiheiit92@mail.ru>
2023-04-03 15:27:17 +0300

    js modal && header

---
+++ app.js
@@ -0,0 +1,98 @@
+$(function() {
+
+    //Modal
+
+    const modalCall = $('[data-modal');
+    const modalClose = $('[data-close');
+
+    modalCall.on('click', function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modal = $this.data('modal');
+
+        $(modal).addClass('modal--show');
+        $('body').addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modal).find(".modal--dialog").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+    });
+
+    modalClose.on('click', function(event) {
+        event.preventDefault();
+
+        let $this = $(this); 
+        let modalParent = $this.parents(".modal");
+
+        modalParent.find(".modal--dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass("modal--show");
+            $("body").removeClass("no-scroll");
+        }, 200);
+    });
+
+   $(".modal").on("click", function(event) {
+
+        let $this = $(this)
+
+        $this.find(".modal--dialog").css({
+            transform: "rotateX(90deg)"
+        });
+
+        setTimeout(function() {
+            $this.removeClass("modal--show");
+            $("body").removeClass("no-scroll");
+        }, 200);
+
+        
+    });
+
+    $(".modal--dialog").on("click", function(event) {
+
+        event.stopPropagation();
+    });
+
+    //Header
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH;
+    let scrollPos = $(window).scrollTop();
+
+    checkScroll(scrollPos, introH);
+
+$(window).on("scroll load resize", function() {
+  introH = intro.innerHeight();
+  scrollPos = $(this).scrollTop();
+
+  checkScroll(scrollPos, introH);
+  
+});
+
+function checkScroll(scrollPos, introH) {
+  if(scrollPos > introH) {
+    header.addClass("fixed");
+  } else {
+    header.removeClass("fixed");
+  }
+}
+
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+
+  let elementID = $(this).data('scroll');
+  let elementOffset = $(elementID).offset().top;
+  
+  console.log(elementOffset);
+
+  $("html, body").animate({
+    scrollTop: elementOffset - 70
+  }, 700);
+});
+})
